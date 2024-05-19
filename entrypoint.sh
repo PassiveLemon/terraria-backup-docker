8df940484b54ead6b4a5186f1392bedbc6ccd436
PassiveLemon <lemonl3mn@protonmail.com>
2024-05-18 22:15:58 -0400

    Refactor: Variable renames

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
-
 function rotator () {
-  WLDNAME=$1
-  COUNT=$2
-  BCKEXT=$3
+  WLD=$1 # World file
+  CNT=$2 # Rotation count
+  EXT=$3 # Backup file extension
   # Delete all backups greater than or equal to the rotation count
-  for BACKUP in /opt/terraria-backup/worlds/Backup/"${WLDNAME}".*"${BCKEXT}"; do
+  for BACKUP in /opt/terraria-backup/worlds/Backup/"${WLD}".*"${EXT}"; do
     # Isolate the backup number
-    BACKUP=$(basename "$BACKUP")
-    BACKUP="${BACKUP#"$WLDNAME".}"
-    BACKUP="${BACKUP%%.*}"
-    if ((BACKUP >= COUNT)); then
-      rm "/opt/terraria-backup/worlds/Backup/$WLDNAME.${BACKUP}${BCKEXT}"
+    BACKUPNUM=$(basename "$BACKUP")
+    BACKUPNUM="${BACKUPNUM#"$WLD".}"
+    BACKUPNUM="${BACKUPNUM%%.*}"
+    if ((BACKUPNUM >= CNT)); then
+      rm "/opt/terraria-backup/worlds/Backup/$WLD.${BACKUPNUM}${EXT}"
     fi
   done
   # Increment all backups by 1 if it exists
-  for ((INC=COUNT; INC >= 0; INC--)); do
-    if [ -e "/opt/terraria-backup/worlds/Backup/$WLDNAME.${INC}${BCKEXT}" ]; then
-      mv "/opt/terraria-backup/worlds/Backup/$WLDNAME.${INC}${BCKEXT}" "/opt/terraria-backup/worlds/Backup/$WLDNAME.$((INC+1))${BCKEXT}"
+  for ((INC=CNT; INC >= 0; INC--)); do
+    if [ -e "/opt/terraria-backup/worlds/Backup/$WLD.${INC}${EXT}" ]; then
+      mv "/opt/terraria-backup/worlds/Backup/$WLD.${INC}${EXT}" "/opt/terraria-backup/worlds/Backup/$WLD.$((INC+1))${EXT}"
     fi
   done
 }
 
-# $1 = Worldname
-# $2 = Extension
+# $1 = World file
 function cpcmd () {
   cp "/opt/terraria-backup/worlds/$1" "/opt/terraria-backup/worlds/Backup/$1.0"
 }
@@ -43,14 +41,14 @@ function 7zipcmd () {
 
 mkdir -p /opt/terraria-backup/worlds/Backup
 
-for WORLD in /opt/terraria-backup/worlds/*.{wld,twld}; do
-  if [ -f "$WORLD" ]; then
-    if [ -z "$WORLD" ]; then
+for FILE in /opt/terraria-backup/worlds/*.{wld,twld}; do
+  if [ -f "$FILE" ]; then
+    if [ -z "$FILE" ]; then
       echo "|| ERROR: No world files detected. ||"
       echo "|| Please ensure you have properly mounted your worlds directory. ||"
       exit 1
     fi
-    WORLD=$(basename "$WORLD")
+    WORLD=$(basename "$FILE")
     # Check if the world is a twld file
     if [ "$WORLD" = "${WORLD%%.*}.twld" ]; then
       WORLD="${WORLD%%.*}.twld"
